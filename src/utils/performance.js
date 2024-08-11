e3568115085a308ec13e98db0e0e27b7eb94f70f
Lars van Vianen <lars@vianen.com>
2024-08-11 12:28:18 +0200

    Create performance.js

---
+++ src/utils/performance.js
@@ -0,0 +1,8 @@
+import Stats from 'stats.js';
+
+export function initStats() {
+    const stats = new Stats();
+    stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+    document.body.appendChild(stats.dom);
+    return stats;
+}
